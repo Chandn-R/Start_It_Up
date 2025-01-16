effb939fc5e4fc64898c3401c2b313cddf974155
Chandn-R <Chandan55.smg@gmail.com>
2025-01-16 21:11:25 +0530

    Day 2: 2025-01-16

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <>
-   <h1 className="text-3xl">Home</h1>
-   </>
+    <>
+      <section className="pink_container">
+        <div className="tag">pitch, vote and grow</div>
+        <h1 className="heading">PITCH YOUR STARTUP, <br />CONNECT WITH ENTREPRENEURS</h1>
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competition</p>
+      </section>
+    </>
   );
 }
